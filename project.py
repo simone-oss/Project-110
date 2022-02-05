4d9adec83c489e5528e5bd362c1ffa487ba104b6
simone-oss <72631613+simone-oss@users.noreply.github.com>
2022-02-04 19:47:08 -0500

    Add files via upload

---
+++ project.py
@@ -0,0 +1,40 @@
+import pandas as pd 
+import plotly.figure_factory as ff 
+import random
+import statistics
+import csv
+import plotly.graph_objects as go
+df=pd.read_csv("data1.csv")
+data=df["reading_time"].tolist()
+#fig=ff.create_distplot([data],["temp"],show_hist=False)
+#fig.show()
+def random_set_of_mean(counter):
+    dataset = []
+    for i in range(0, counter):
+        random_index= random.randint(0,len(data)-1)
+        value = data[random_index]
+        dataset.append(value)
+    mean = statistics.mean(dataset)
+    return mean
+
+#function to plot the mean on the graph
+def show_fig(mean_list):
+    df = mean_list
+    mean = statistics.mean(df)
+    fig = ff.create_distplot([df], ["reading_time"], show_hist=False)
+    fig.add_trace(go.Scatter(x=[mean, mean], y=[0, 1], mode="lines", name="MEAN"))
+    fig.show()
+
+
+# Pass the number of time you want the mean of the data points as a parameter in range function in for loop
+def setup():
+    mean_list = []
+    for i in range(0,1000):
+        set_of_means= random_set_of_mean(100)
+        mean_list.append(set_of_means)
+    show_fig(mean_list)
+    
+    mean = statistics.mean(mean_list)
+    print("Mean of sampling distribution :-",mean )
+
+setup()
